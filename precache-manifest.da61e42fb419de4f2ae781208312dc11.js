6753fc4c6bc8c15690d7c41410dfc52f5c6a7ffb
= <=>
2020-04-30 16:40:58 +0530

    Updates

---
+++ precache-manifest.da61e42fb419de4f2ae781208312dc11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20b4c01360c8085eb517ec12985d44bb",
+    "url": "/hect-to-bigha-converter-app/index.html"
+  },
+  {
+    "revision": "79eabfd9e764cd1acc76",
+    "url": "/hect-to-bigha-converter-app/static/css/main.33037c13.chunk.css"
+  },
+  {
+    "revision": "c8a777feb92a3aac7f96",
+    "url": "/hect-to-bigha-converter-app/static/js/2.0a37d8b4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/hect-to-bigha-converter-app/static/js/2.0a37d8b4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "79eabfd9e764cd1acc76",
+    "url": "/hect-to-bigha-converter-app/static/js/main.b359bba7.chunk.js"
+  },
+  {
+    "revision": "f5a68631139729676b49",
+    "url": "/hect-to-bigha-converter-app/static/js/runtime-main.8dded91c.js"
+  }
+]);
